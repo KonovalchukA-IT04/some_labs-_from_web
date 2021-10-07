510c5e7f91fa332dec9e8d7009187e69e3506027
KonovalchukA-IT04 <76063066+KonovalchukA-IT04@users.noreply.github.com>
2021-10-07 23:04:27 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -61,6 +61,10 @@ function FindArea() {
 window.onload = CookiesReload();
 function CookiesReload()
 {
+    if(document.cookie='')
+    {
+        return;
+    }
     let de_way = confirm("Coowkies UwU: "+document.cookie+"\nSawe_it?")
     if(de_way)  
 	{
