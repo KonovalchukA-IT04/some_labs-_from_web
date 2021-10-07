14dc5d77032d4d64347ac6cb1f86ee5c301d3c27
KonovalchukA-IT04 <76063066+KonovalchukA-IT04@users.noreply.github.com>
2021-10-07 22:59:34 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -58,9 +58,54 @@ function FindArea() {
 }
 
 //3-rd task
+window.onload = CookiesReload();
+function CookiesReload()
+{
+    let de_way = confirm("Coowkies UwU: "+document.cookie+"\nSawe_it?")
+    if(de_way)  
+	{
+		let de_way2 = confirm("This site uses coowkies.\nWanna restawt UwU?");
+		if(de_way2)
+		{
+			window.location.reload();
+		}
+        else{
+            deleteCookie("dividers");
+            FunkyGeneration();
+        }
+	}
+    else{
+        deleteCookie("dividers");
+        FunkyGeneration();
+    }
+}
+
+function FunkyGeneration(){    
+    var tag1 = document.createElement("form");
+    tag1.name = "dividers";
+    tag1.action="";    
+
+    var tag2 = document.createElement("lable");
+    var text = document.createTextNode("Enter here: ");
+    tag2.appendChild(text);
+
+    var tag3 = document.createElement("input");
+    tag3.type = "text";
+    tag3.id = "enterarea";
+
+    var tag4 = document.createElement("input");
+    tag4.type = "button";
+    tag4.id = "enterbutton";
+    tag4.value = "enter";
+
+    tag1.appendChild(tag2);
+    tag1.appendChild(tag3);
+    tag1.appendChild(tag4);
+    table_5.appendChild(tag1);
+}
+
 let butt = document.getElementById("enterbutton");
 let entarea = document.getElementById("enterarea");
-
 butt.onclick = function(event){
     var arr=[];
     for (var i = 0, length = entarea.value; i <= length; i++)
@@ -81,9 +126,10 @@ butt.onclick = function(event){
 
 function setCookie(name, value, options = {}) {
   
-     options = { 
-        path: '/' 
-    };
+    /*options = { 
+        path: '/',
+        domain: '127.0.0.1'
+    };*/
 	if (options.expires instanceof Date) 
 	{
 	  options.expires = options.expires.toUTCString();
@@ -168,3 +214,100 @@ function Startcase()
     }
 }
 Startcase();
+
+//5-th task
+let x_conteiner = document.getElementById("x");
+var lol_dont_press_pls = 0;
+x_conteiner.onclick = function(event){
+    if(lol_dont_press_pls==0)
+    {
+        ImgFormGen();
+    }
+    lol_dont_press_pls++;
+}
+
+function ImgFormGen(){
+    var tag1 = document.createElement("form");
+    tag1.name = "imgforms";
+    tag1.action="";
+
+    var tag2 = document.createElement("input");
+    tag2.type = "button";
+    tag2.id = "buttonadd";
+    tag2.value = "Show";
+
+    var tag3 = document.createElement("input");
+    tag3.type = "button";
+    tag3.id = "buttonsave";
+    tag3.value = "Save";
+
+    var tag4 = document.createElement("input");
+    tag4.type = "text";
+    tag4.id = "pictureurl";
+
+    tag1.appendChild(tag2);
+    tag1.appendChild(tag3);
+    tag1.appendChild(tag4);
+    table_5.appendChild(tag1);
+    var div1 = document.createElement("div");
+    div1.style="display: flex; flex-direction: column;";    
+    document.getElementById("n4").appendChild(div1);
+
+    tag3.onclick = function(event){
+        var div2 = document.createElement("div");
+        div2.style="display: flex; flex-direction: column;";   
+
+        var img1 = document.createElement("img");
+        img1.src = tag4.value;
+        img1.alt = "imgnotfound(";
+        img1.style = "height: 100px; width: 100px;"
+        div2.appendChild(img1);
+        tag4.value="";
+        var urimg = localStorage.getItem("img")+"@"+img1.src;
+        localStorage.setItem("img", urimg);
+
+        var img2 = document.createElement("button");
+        img2.name="delbut";
+        img2.textContent="delete?"
+        img2.style="width:100px; height:25px;"
+        img2.id=img1.src;
+        div2.appendChild(img2);
+        div1.appendChild(div2);
+
+        img2.onclick = function(event){
+            img2.parentNode.remove();
+            localStorage.setItem("img", localStorage.getItem("img").replace('@'+img2.id,''));
+        }
+    }
+
+    tag2.onclick = function(event){
+        div1.innerHTML='';
+        var arr = localStorage.getItem("img").split('@');
+        arr.forEach(element => {
+            if(element!='null'){            
+            
+                var div2 = document.createElement("div");
+                div2.style="display: flex; flex-direction: column;";   
+
+                var img1 = document.createElement("img");
+                img1.src = element;
+                img1.alt = "imgnotfound(";
+                img1.style = "height: 100px; width: 100px;"
+                div2.appendChild(img1);
+
+                var img2 = document.createElement("button");
+                img2.textContent="delete?"
+                img2.name="delbut";
+                img2.style="width:100px; height:25px;"
+                img2.id=img1.src;
+                div2.appendChild(img2);
+                div1.appendChild(div2);
+
+                img2.onclick = function(event){
+                    img2.parentNode.remove();
+                    localStorage.setItem("img", localStorage.getItem("img").replace('@'+img2.id,''));
+                }
+            }
+        });
+    }
+}
